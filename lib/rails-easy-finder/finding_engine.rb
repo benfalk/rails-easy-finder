ffc0dc08053968f09c2fd40d366e7aebf4459fb4
Benjamin Falk <benjamin.falk@yahoo.com>
2013-03-15 08:47:03 -0500

    in the middle of refactoring for dynamic decisions

--- lib/rails-easy-finder/finding_engine.rb
+++ lib/rails-easy-finder/finding_engine.rb
@@ -4,9 +4,24 @@ module Rails
     # Generates a finder set to be used
     class FindingEngine < Object
 
-      def initialize(&block)
-        @scopes = []
-        instance_eval &block
+      def initialize(scope,binding = nil,&block)
+        @scope = scope
+        bind binding if binding
+        uses_the_solution &block if block_given?
+      end
+
+      def bind(obj)
+        @bind = obj
+      end
+
+      def uses_the_solution(&block)
+        @solution = block
+      end
+
+      def results
+        @scoped = @scope.scoped
+        instance_eval &@solution
+        @scoped
       end
 
       private
@@ -17,7 +32,12 @@ module Rails
 
       def build_param_predicate(param)
         define_singleton_method "the_#{param}" do |field = nil|
-          field ? "#{param}.#{field}" : param.to_s
+          field ? @bind.send(param).send(field) : @bind.send(param)
+          #field ? "#{param}.#{field}" : param.to_s
+        end
+        singleton_class.class_eval do
+          alias_method "of_the_#{param}", "the_#{param}"
+          alias_method "for_the_#{param}", "the_#{param}"
         end
       end
 
@@ -25,10 +45,9 @@ module Rails
         if matches_param_predicate? method
           method.to_s =~ /the_([_a-z]*)/
           build_param_predicate $1.to_sym
-
+          send "the_#{$1}", *parms
         else
-          @scopes << [method,parms]
-          p @scopes
+          @scoped = @scoped.send method, *parms
         end
       end
 
--- spec/rails-easy-finder/finding_engine_spec.rb
+++ spec/rails-easy-finder/finding_engine_spec.rb
@@ -5,12 +5,52 @@ module Rails
 
     describe FindingEngine do
 
+      before :each do
+        @scope = double('scope')
+        @scope.as_null_object
+      end
+
       it 'does not blow up' do
-        @engine = FindingEngine.new do
+        @engine = FindingEngine.new(@scope) do
           willing_to_travel_to the_job_request(:address)
           available_on the_job_request(:time)
+          trying_to_live for_the_money
+          where :anything => for_the_love_of_money
         end
+
+        @bind = double('bind')
+
         @engine.the_job_request(:address).should == 'job_request.address'
+
+        @another = FindingEngine.new(@scope)
+        @another.uses_the_solution do
+          who_loves the_person
+        end
+      end
+
+      context 'when bound to a scopeable searcher' do
+
+        before :each do
+          @obj = double('scope')
+          @obj.as_null_object
+          @engine = FindingEngine.new(@scope)
+          @engine.bind(@obj)
+        end
+
+        it 'can fetch results from it' do
+          @obj.should_receive(:scoped).and_return(@obj)
+          @engine.results.should be(@obj)
+        end
+
+        it 'calls scoped searches from the bound object' do
+          @obj.should_receive(:tested_by).and_return(@obj)
+          @obj.should_receive(:person).and_return('test')
+          @engine.uses_the_solution do
+            tested_by the_person
+          end
+          @engine.results.should be(@obj)
+        end
+
       end
 
 
